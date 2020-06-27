dbac04b9bbe5d2437dea14161bc94b93cbe423d8
Ling.A <3346711261@qq.com>
2020-06-27 17:42:35 +0800

    first commit

---
+++ src/mock/profile.js
@@ -0,0 +1,13 @@
+export default {
+    profile:{
+        "code" : "10000",
+        "success" : true,
+        "message" : "登录成功",
+        "data" : {
+            mobile:"138000138000",
+            username:"张三ok",
+            company:'lingtianxing公司',
+            email:'xxxxxxxxx@qq.com',
+        }
+    }
+}
